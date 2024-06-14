f3817ce859a7e0275a611a6f75acd7fc4ccb3708
JRDittenber <jeff.dittenber@hotmail.com>
2024-06-14 13:27:41 -0500

    installed requirements and run setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative AI Project: Interview Question Generator',
+    author = 'J. Dittenber',
+    author_email='jeff.dittenber@hotmail.com',
+    packages=find_packages(),
+    version='0.0.0',
+    install_requires =[]
+)
+
